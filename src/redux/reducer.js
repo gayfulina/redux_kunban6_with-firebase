79dd5155248c8f5ff17bf071627aca6fa9c271e3
Irina Gayfulina <63535542+gayfulina@users.noreply.github.com>
2022-12-13 16:27:44 -0800

    wip

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,13 +1,7 @@
 const initialState = {
     cards: [],
     statuses: [],
-    priorities: [
-        {priority: 1},
-        {priority: 2},
-        {priority: 3},
-        {priority: 4},
-        {priority: 5},
-    ],
+    // priorities: [],
     title: 'Kanban board'
 }
 
